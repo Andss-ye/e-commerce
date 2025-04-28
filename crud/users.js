205f8cdb263af7b45a3513524c2406d2a99d239f
Andss-ye <andrex120tv@gmail.com>
2025-04-27 23:11:07 -0500

    Agregar funciones CRUD para usuarios: se creó el archivo users.js con las funciones para crear, obtener, actualizar y eliminar usuarios.

---
+++ crud/users.js
@@ -0,0 +1,15 @@
+export async function createUser(userData) {
+    return 1;
+}
+
+export async function getUserById(id) {
+    return 1;
+}
+
+export async function updateUser(id, updateData) {
+    return 1;
+}
+
+export async function deleteUser(id) {
+    return 1;
+}
